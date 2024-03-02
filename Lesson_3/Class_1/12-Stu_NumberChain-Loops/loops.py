d9da95e2318725b24f6f0aec77f5f1d54f451773
Jack Temme <jtemme7@gmail.com>
2024-03-02 13:53:14 -0700

    Update loops.py

--- Lesson_3/Class_1/12-Stu_NumberChain-Loops/loops.py
+++ Lesson_3/Class_1/12-Stu_NumberChain-Loops/loops.py
@@ -1,10 +1,10 @@
-user_input = "y"
+proceed = "y"
 
-while user_input == "y":
-    num = input("How many numbers")
-
-    for x in range(int(num)):
-        print (x)
-
-user_input = input("continue: (y)es or (n)o")
-
+while proceed == "y":
+    num = input("How many numbers do you want to print out? ")
+    
+    num = int(num) + 1
+    for x in range(num):
+        print(x)
+    proceed = input("Do you want to continue: (y)es or (n)o ")
+    
